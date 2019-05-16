b881356dcef3c5acfc21f80dacf13f1b96c1737a
Ilya Sukhoplyuev <suhoy95@gmail.com>
2019-05-16 23:14:44 +0300

    add cpuid functionality

---
+++ Sources/dumplier/cpuid.h
@@ -0,0 +1,8 @@
+#ifndef _CPUID_H
+#define _CPUID_H
+
+#include "config.h"
+
+int do_intel(void);
+
+#endif // _CPUID_H
--- Sources/dumplier/kernel.c
+++ Sources/dumplier/kernel.c
@@ -1,12 +1,12 @@
 #include "config.h"
 #include "stdio.h"
+
 #include "vga-terminal.h"
+#include "cpuid.h"
 
 void kernel_main(void)
 {
     vga_terminal_initialize();
 
-    for (size_t i = 0; i < 30; i++) {
-        printf("Hello %d, kernel %s!\n", i, "World");
-    }
+    do_intel();
 }
